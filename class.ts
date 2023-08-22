4af3f8f2bf02140702dbdd608efc28ab45a1ab04
agenious1122 <141574760+agenious1122@users.noreply.github.com>
2023-08-23 01:19:18 +0500

    add subtract

--- class.ts
+++ class.ts
@@ -1,10 +1,12 @@
+//sum
 var num1:number=5
 var num2:number=6
 var result=num1+num2
 console.log(result)
-
+//subtract
 var num1:number=5
 var num2:number=6
-console.log(num1+num2)
+var result=num2-num1
+console.log(result)
 
 
